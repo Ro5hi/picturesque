90f46eb80f3b5aca038ae074d7fe0a71ff3901fe
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-08 18:55:00 -0400

    added helper methods, comments methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,23 @@
 class ApplicationController < ActionController::Base
 
-    def current_user
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    helper_method :current_user, :logged_in, :invalid_user
+
+    def index 
+        redirect_to root_path
     end 
 
-    helper_method :current_user
+    def logged_in
+        !!current_user
+    end 
+
+    def current_user
+        @user ||= User.find(session[:user_id])
+    end     
+
+    def invalid_user
+        if !logged_in 
+            redirect_to new_user_session_path
+        end 
+    end
     
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+    # User comments about photos go here 
+
+    def show 
+    end 
+
+    def create 
+    end 
+
+    def update 
+    end 
+
+    def destroy 
+    end 
+    
 end
