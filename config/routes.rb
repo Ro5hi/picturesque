b9217a643bc2d4bb42b0e35a62d96ff3101672e6
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-10 17:15:09 -0400

    debugging upload, permission errors

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
     get '/auth/twitter' => 'devise/omniauth_callbacks#passthru'
     get '/auth/twitter/callback' => 'omniauth_callbacks#twitter'
-  end
+  end 
 end 
