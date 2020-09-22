a6c4a0a5db51dfca4374f1a0cca7d535797fccaa
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-22 12:39:14 -0400

    fixed routes to bring up logout/edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < ApplicationController
 
     before_action :set_profile, only: [:show]
+    before_action :profile_photo, only: [:show]
 
     def show
+        photo = params[:photo]
     end  
 
     def index
         @posts = Post.active.order("created_at DESC")
+    end
+
+    def edit
+        redirect_to edit_user_registration_path 
     end 
 
     private 
 
     def set_profile
         @user = User.find_by_username(params[:username])
+    end
+
+    def profile_photo 
+        @user = User.find_by(params[:photo])
     end
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,24 @@ Rails.application.routes.draw do
 
   root 'users#index'  
 
-  resources :tags, only: [:index, :show]
-
-  resources :users do 
-    resources :posts
-  end
-
-  resources :posts, shallow: true do 
-    resources :comments
-  end 
-
-  get '/tag/:name' => "tags#show"
-  get '/:username/posts/:id' => "posts#show"
-  get '/profile/:username' => "users#show", as: :profile
-  
   devise_for :users, controllers:{omniauth_callbacks: "omniauth_callbacks"}
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
     get '/auth/twitter' => 'devise/omniauth_callbacks#passthru'
     get '/auth/twitter/callback' => 'omniauth_callbacks#twitter'
   end
+
+  resources :users, only: [:index, :show] do
+    resources :posts
+  end
+  get '/profile/:username' => "users#show", as: :profile 
+  
+  resources :posts, shallow: true do 
+    resources :comments
+  end 
+
+  resources :tags, only: [:index, :show]
+  get '/tag/:name' => "tags#show"
+  get '/:username/posts/:id' => "posts#show"
+  
 end
